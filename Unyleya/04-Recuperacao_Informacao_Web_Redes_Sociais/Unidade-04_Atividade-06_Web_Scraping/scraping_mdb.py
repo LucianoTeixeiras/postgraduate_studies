22fb026c86634058613924a3b81505dfc77f92e1
Luciano Teixeira <luciano.teixeiras@gmail.com>
2022-12-25 19:17:52 +0000

    Add web scraping app

--- Unyleya/04-Recuperacao_Informacao_Web_Redes_Sociais/Unidade-04_Atividade-06_Web_Scraping/scraping_mdb.py
+++ Unyleya/04-Recuperacao_Informacao_Web_Redes_Sociais/Unidade-04_Atividade-06_Web_Scraping/scraping_mdb.py
@@ -1,1 +1,15 @@
-# Libs to be Import
+# Import Libs
+from bs4 import BeautifulSoup
+import requests
+
+# Definindo a URL
+url = "https://www.imdb.com/search/title/?count=100&groups=top_1000&sort=user_rating"
+
+# Request do conteudo da URL
+url_text = requests.get(url).text
+
+# Parse do conteudo
+url_texts = BeautifulSoup(url_text, 'html.parser')
+
+for text in url_texts.find_all('a'):
+    print(text.get('href'))
